810eb7ae0897358932cdf153e20898710b2f3c3b
Eli <hellan_nikolova@abv.bg>
2015-08-05 21:30:35 +0300

    Fix js code so that slideshow next/previous buttons can work.

--- js/backgroundTest.js
+++ js/backgroundTest.js
@@ -1,5 +1,6 @@
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
+        if ($(this).attr("href") == "#myCarousel") return; //This is the exception
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -12,6 +13,7 @@ $(function() {
         }
     });
 });
+
 
 $.fn.extend({
     toggleText: function(text2, text1) {
